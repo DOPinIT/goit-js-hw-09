04dadc17332b804c920f405228897cbe7e781211
DOPinIT <111194712+DOPinIT@users.noreply.github.com>
2023-02-03 16:01:05 +0200

    add starter files

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,19 @@
+
+
+const btn = document.querySelectorAll("button");
+
+const body = document.querySelector("body");
+
+console.log(body);
+btn.forEach(btn => {
+    if (condition) {
+        
+    }
+})
+
+
+console.log(btn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
