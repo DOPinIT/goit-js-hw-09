b4c7564f14eab7e16e4c5ddc3d24789bcab52f8a
DOPinIT <111194712+DOPinIT@users.noreply.github.com>
2023-02-07 13:35:36 +0000

    Deploying to gh-pages from @ DOPinIT/goit-js-hw-09@abb8d3af060bebdd00d1e31f2aae01610b29d1f9 🚀

--- 01-color-switcher.ed872d37.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelectorAll("button"),e=document.querySelector("body");console.log(e),o.forEach((o=>{condition})),console.log(o);
-//# sourceMappingURL=01-color-switcher.ed872d37.js.map
