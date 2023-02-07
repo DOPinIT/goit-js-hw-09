abb8d3af060bebdd00d1e31f2aae01610b29d1f9
DOPinIT <illyakonor@gmail.com>
2023-02-07 15:34:51 +0200

    qwer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,20 +1,29 @@
-// CONST
 
 
-const btn = document.querySelectorAll("button");
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let changeBodyColorId = null;
 
-const body = document.querySelector("body");
-
-console.log(body);
-btn.forEach(btn => {
-    if (condition) {
-        
-    }
-})
+startBtn.addEventListener('click', onClickStartBtn);
+stopBtn.addEventListener('click', onClickStopBtn);
 
 
-console.log(btn);
+function onClickStartBtn() {
+  stopBtn.disabled = false;
+    startBtn.disabled = true;
+    changeBodyColorId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+};
+
+function onClickStopBtn() {
+  stopBtn.disabled = true
+    startBtn.disabled = false;
+    clearInterval(changeBodyColorId);
+};
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
